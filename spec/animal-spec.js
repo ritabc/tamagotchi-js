1c9d56a818ba310977694af144acc90158f23ddc
Rita Bennett-Chew and Renee Sarley <rita@>
2018-08-06 11:30:32 -0700

    add prototype for decreasing characteristic levels

--- spec/animal-spec.js
+++ spec/animal-spec.js
@@ -5,35 +5,58 @@ describe('Animal', function(){
 
   beforeEach(function(){
     jasmine.clock().install();
-    // newAnimal.increase(newAnimal.hunger);
-    // newAnimal.increase(newAnimal.activityNeeds);
-    // newAnimal.increase(newAnimal.sleepiness);
-
   });
 
   afterEach(function() {
     jasmine.clock().uninstall();
-    // this.hunger.currentLevel = 50
+    newAnimal.hunger.currentLevel = 50
+    newAnimal.activity.currentLevel = 50
+    newAnimal.sleepiness.currentLevel = 50
   });
 
-  it('should have a food level of 70 after 30 seconds', function(){
+  it('should have a food level of 70 after a regularIncrease and 30 seconds', function(){
     newAnimal.regularIncrease(newAnimal.hunger);
     jasmine.clock().tick(30001);
     expect(newAnimal.hunger.currentLevel).toEqual(70);
+  });
+
+  it('should have a food level of 70 after a fastIncrease and 30 seconds', function(){
+    newAnimal.fastIncrease(newAnimal.hunger);
+    jasmine.clock().tick(30001);
+    expect(newAnimal.hunger.currentLevel).toEqual(80);
+  });
+
+  it('should have a food level of 70 after a slowIncrease and 30 seconds', function(){
+    newAnimal.slowIncrease(newAnimal.hunger);
+    jasmine.clock().tick(30001);
+    expect(newAnimal.hunger.currentLevel).toEqual(60);
+  });
+
+  it('should have a activity level of 75 after a regularIncrease and 45 seconds', function(){
+    newAnimal.regularIncrease(newAnimal.activity);
+    jasmine.clock().tick(45001);
+    expect(newAnimal.activity.currentLevel).toEqual(75);
+  });
+
+  it('should have a sleepiness level of 65 after a regularIncrease and 60 seconds', function(){
+    newAnimal.regularIncrease(newAnimal.sleepiness);
+    jasmine.clock().tick(60001);
+    expect(newAnimal.sleepiness.currentLevel).toEqual(65);
+  });
+
+  it('should decrease the current level of hunger by its increment value of 20, resulting in current level of 30', function(){
+    newAnimal.regularDecrease(newAnimal.hunger);
+    expect(newAnimal.hunger.currentLevel).toEqual(30);
   })
 
-  // it('should have a activity needs level of 60 after 45 seconds', function(){
-  //   jasmine.clock().tick(45001);
-  //   expect(newAnimal.activityNeeds.currentLevel).toEqual(60);
-  // })
-  //
-  // it('should have a sleepiness level of 65 after 60 seconds', function(){
-  //   jasmine.clock().tick(60001);
-  //   expect(newAnimal.sleepiness.currentLevel).toEqual(65);
-  // })
-  //
-  //
-  // let newAnimal = new Animal("Bob", "Pig", 5);
-  // newAnimal.fastIncrease(sleepiness);
+  it('should decrease the current level of hunger by its increment value of 20 + 10, resulting in current level of 20', function(){
+    newAnimal.fastDecrease(newAnimal.hunger);
+    expect(newAnimal.hunger.currentLevel).toEqual(20);
+  })
+
+  it('should decrease the current level of hunger by its increment value of 20 - 10, resulting in current level of 40', function(){
+    newAnimal.slowDecrease(newAnimal.hunger);
+    expect(newAnimal.hunger.currentLevel).toEqual(40);
+  })
 
 });
--- src/animal.js
+++ src/animal.js
@@ -3,22 +3,27 @@ import { Characteristic } from './../src/characteristic.js';
 export function Animal(name, type){
   this.name = name;
   this.type = type;
-  // instantiate new instance of Characteristic called hunger
   this.hunger = new Characteristic('hunger', 50, 30000, 20)
-
-
-  // this.hunger = {currentLevel: 50, interval: 30000, increment: 20}
-  this.activityNeeds = {currentLevel: 50, interval: 45000, increment: 10}
-  this.sleepiness = {currentLevel: 50, interval: 60000, increment: 15}
-  this.fastIncrease = this.increase(10);
-  this.regularIncrease = this.increase(0);
-  this.slowIncrease = this.increase(-10)
+  this.activity = new Characteristic('activity', 50, 45000, 25)
+  this.sleepiness = new Characteristic('sleepiness', 50, 60000, 15)
+  this.fastIncrease = this.needsIncrease(10);
+  this.regularIncrease = this.needsIncrease(0);
+  this.slowIncrease = this.needsIncrease(-10)
+  this.fastDecrease = this.needsSatisfied(10);
+  this.regularDecrease = this.needsSatisfied(0);
+  this.slowDecrease = this.needsSatisfied(-10);
 }
 
-Animal.prototype.increase = function(incrementBoost) {
+Animal.prototype.needsIncrease = function(incrementBoost) {
   return function(property){
     setInterval(() => {
       property.currentLevel += property.increment + incrementBoost;
     }, property.interval);
+  }
+}
+
+Animal.prototype.needsSatisfied = function(decrementBoost) {
+  return function(property){
+    property.currentLevel -= property.increment + decrementBoost;
   }
 }
